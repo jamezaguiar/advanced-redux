9aa07a531f4a41f49b15420b0fabf4e4cb24af05
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-12-06 22:02:05 -0300

    sync cart with useEffect

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -5,7 +6,15 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_FIREBASE_URL}/cart.json`, {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
